ef293672f1142b7699b04777a3aea94728b9ab9e
OlgaKovalevskayaa <krishtaoivicho@mail.ru>
2023-02-12 21:54:05 +0300

    hometask-9

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const timerShow = document.querySelector("#pomodoro-time");
 const buttonStart = document.querySelector("#start");
-buttonStart.addEventListener("click", start)
-let idInterval;
+const buttonBreak = document.querySelector("#break");
+const buttonPomodoro = document.querySelector("#pomodoro");
+const buttonReset = document.querySelector("#reset");
+let minutes = 25;
+let seconds = 00;
+let idInterval = null;
+const pomodoroTime = document.querySelector("#pomodoro-time");
 
+buttonStart.addEventListener("click", start)
 
 function start() {
     buttonStart.textContent = "stop";
-    idInterval = setInterval(funcName, 10);
+    idInterval = setInterval(funcName, 100);
     this.removeEventListener('click', start);
     this.addEventListener('click', stop);
 }
@@ -18,10 +24,9 @@ function stop() {
     this.addEventListener('click', start);
 };
 
-let minutes = 25;
-let seconds = 00;
+
 const funcName = function() {
-    if (minutes == 0 && seconds == 0) {
+    if (minutes === 0 && seconds === 0) {
         clearInterval(idInterval);
         minutes = 25;
         seconds = 0;
@@ -42,3 +47,43 @@ const funcName = function() {
     }
     timerShow.textContent = `${format(minutes)} : ${format(seconds)}`;
 };
+
+buttonBreak.addEventListener("click", function breakMode() {
+    clearInterval(idInterval);
+    buttonStart.textContent = "start";
+    minutes = 5;
+    seconds = 0;
+    pomodoroTime.textContent = '05 : 00'
+
+    return buttonReset.addEventListener("click", function() {
+        clearInterval(idInterval);
+        buttonStart.textContent = "start";
+        minutes = 5;
+        seconds = 0;
+        pomodoroTime.textContent = '05 : 00'
+    })
+})
+
+buttonPomodoro.addEventListener("click", function tomatoMode() {
+    clearInterval(idInterval);
+    buttonStart.textContent = "start";
+    minutes = 25;
+    seconds = 0;
+    pomodoroTime.textContent = '25 : 00'
+
+    return buttonReset.addEventListener("click", function() {
+        clearInterval(idInterval);
+        buttonStart.textContent = "start";
+        minutes = 25;
+        seconds = 0;
+        pomodoroTime.textContent = '25 : 00'
+    })
+})
+
+buttonReset.addEventListener("click", function() {
+    clearInterval(idInterval);
+    buttonStart.textContent = "start";
+    minutes = 25;
+    seconds = 0;
+    pomodoroTime.textContent = '25 : 00'
+})
